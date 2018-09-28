eee11eb81cd02d349f07dfa2f917ff1655696eb2
Frances Yu <francesyu90@yahoo.com>
2018-09-28 05:57:28 -0700

    Enabled more button to open a dialog

--- myreads/src/components/BookContainer.js
+++ myreads/src/components/BookContainer.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
+import Typography from '@material-ui/core/Typography';
+
+import CustomizedDialog from './CustomizedDialog';
+
+const emails = ['username@gmail.com', 'user02@gmail.com'];
+
 class BookContainer extends Component {
+
+    state = {
+        open: false,
+        selectedValue: emails[1],
+    };
+    
+    handleClickOpen = () => {
+        this.setState({
+            open: true,
+        });
+    };
+    
+    handleClose = value => {
+        this.setState({ selectedValue: value, open: false });
+    };
 
     render() {
 
@@ -22,12 +44,23 @@ class BookContainer extends Component {
                         title={book.title}
                         subtitle={<span>by: {book.authors} </span>}
                         actionIcon={
-                            <IconButton style={{color: 'rgba(255, 255, 255, 0.54)'}}>
+                            <IconButton 
+                                style={{color: 'rgba(255, 255, 255, 0.54)'}}
+                                onClick={this.handleClickOpen}
+                            >
                                 <MoreVertIcon />
                             </IconButton>
                         }
                     />
                 </GridListTile>
+
+                <Typography variant="subheading">Selected: {this.state.selectedValue}</Typography>
+                <br />
+                <CustomizedDialog
+                    selectedValue={this.state.selectedValue}
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                />
             </div>
         );
     }

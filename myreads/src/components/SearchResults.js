9b0f984e50e7098b73035cb3895ef42f1a389002
Frances Yu <francesyu90@yahoo.com>
2018-09-28 00:26:21 -0700

    Handled empty search results (contd)

--- myreads/src/components/SearchResults.js
+++ myreads/src/components/SearchResults.js
@@ -20,7 +20,8 @@ class SearchResults extends Component {
     render() {
         return(
             <div>
-                { this.state.books.length > 0 && <BooksContainer books={this.state.books} />}
+                { this.state.books && this.state.books.length === 0 && alert("No matched books is found.")}
+                { this.state.books && this.state.books.length > 0 && <BooksContainer books={this.state.books} />}
             </div>
         );
     }

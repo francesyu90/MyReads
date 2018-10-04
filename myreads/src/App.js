a750e7f7e43aa10105436047e4061404a2476205
Frances Yu <francesyu90@yahoo.com>
2018-10-04 12:14:26 -0700

    fix: Rewrote App component to pass down all books

--- myreads/src/App.js
+++ myreads/src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import * as BooksAPI from './api/BooksAPI';
+
 import CustomizedNavbar from './components/CustomizedNavbar';
 import { Route } from 'react-router-dom';
 import SearchPage from './pages/SearchPage';
 import MainPage from './pages/MainPage';
 
 class App extends Component {
+
+    state = {
+        allBooks: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState(() => ({
+                allBooks: books
+            }));
+        })
+    }
+
     render() {
+
+        const { allBooks } = this.state;
+
         return (
             <div className="App">
+
                 <CustomizedNavbar />
-                <Route exact path = "/" component={MainPage} />
-                <Route exact path = "/search" component={SearchPage} />
+
+                <Route exact path = "/" render={() => (
+                    <MainPage allBooks={allBooks} />
+                )} />
+                
+                <Route exact path = "/search" render={() => (
+                    <SearchPage allBooks={allBooks} />
+                )} />
             </div>
         );
     }
